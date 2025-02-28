2036af69caf1d6c22e3be8add8fe6b1dd1e76f34
flytachi <jasur.rakhmatov03@gmail.com>
2025-02-28 17:15:25 +0500

    realise 5.2.3

--- src/Http/Rendering.php
+++ src/Http/Rendering.php
@@ -43,7 +43,7 @@ final class Rendering
                 $resource->getResource()
             );
         } elseif ($resource instanceof \Throwable) {
-            $this->httpCode = HttpCode::tryFrom($resource->getCode()) ?: HttpCode::UNKNOWN_ERROR;
+            $this->httpCode = HttpCode::tryFrom((int) $resource->getCode()) ?: HttpCode::UNKNOWN_ERROR;
             $this->logging($resource);
             $this->header = ExceptionWrapper::wrapHeader();
             $this->body = ExceptionWrapper::wrapBody($resource);
