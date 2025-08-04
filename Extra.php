37eba0ff15a190cd9a346c155310daa7d33f5e90
flytachi <jasur.rakhmatov03@gmail.com>
2025-08-04 17:06:38 +0500

    demo realise 5.7.x

--- Extra.php
+++ Extra.php
@@ -55,6 +55,10 @@ final class Extra extends ExtraConfig
             ini_set('error_reporting', E_ALL);
             ini_set('display_errors', 1);
             ini_set('display_startup_errors', 1);
+        } else {
+            ini_set('error_reporting', 0);
+            ini_set('display_errors', 0);
+            ini_set('display_startup_errors', 0);
         }
 
         // logger
